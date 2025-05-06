2a671266371aac72ee9a1ff4db0b8eba2b86c2f3
Val <alu0101485287@ull.edu.es>
2025-05-06 18:47:03 +0100

    doc: add TSDoc comments on `hunter.ts`

--- src/models/hunter.ts
+++ src/models/hunter.ts
@@ -1,6 +1,9 @@
-import { Document, connect, model, Schema } from 'mongoose';
+import { Document, model, Schema } from 'mongoose';
 import validator from 'validator';
 
+/**
+ * Union type representing all valid hunter races.
+ */
 export type Race =
   | "Human"
   | "Elf"
@@ -13,6 +16,10 @@ export type Race =
   | "Spectral Cat"
   | "Half-Elf";
 
+  /**
+   * Array of all acceptable race values, used for validation and enum constraint.
+   * @type {const}
+   */
 export const RaceValues = [
   "Human",
   "Elf",
@@ -26,12 +33,26 @@ export const RaceValues = [
   "Half-Elf",
 ] as const;
 
+/**
+ * Interface representing the structure of a Hunter document in MongoDB.
+ * This interface extends the Mongoose Document interface to include custom fields.
+ * @interface HunterDocumentInterface
+ * @extends Document
+ * @property {string} name - The name of the hunter.
+ * @property {race} race - The race of the hunter, which must be one of the predefined values.
+ * @property {string} location - The location of the hunter.
+ */
 interface HunterDocumentInterface extends Document {
   name: string,
   race: Race,
   location: string
 }
 
+/**
+ * Mongoose schema definition for the Hunter model.
+ * This schema defines the structure of the Hunter documents in MongoDB.
+ * @type {Schema<HunterDocumentInterface>}
+ */
 const HunterSchema = new Schema<HunterDocumentInterface>({
   name: {
     type: String,
@@ -73,4 +94,10 @@ const HunterSchema = new Schema<HunterDocumentInterface>({
   }
 });
 
+/**
+ * Mongoose model for the Hunter schema.
+ * This model provides an interface to interact with the Hunter collection in MongoDB.
+ * @type {model<HunterDocumentInterface>}
+ * @remarks Collection name: 'Hunters'.
+ */
 export const Hunter = model<HunterDocumentInterface>('Hunters', HunterSchema);
