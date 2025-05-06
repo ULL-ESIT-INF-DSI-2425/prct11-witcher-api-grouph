6495bc411f41a108320d23991c70af030a6045c8
Airam Prieto <alu0101546377@ull.edu.es>
2025-05-06 15:44:08 +0000

    fix: refactor  code.

--- src/routers/merchantRouter.ts
+++ src/routers/merchantRouter.ts
@@ -1,99 +1,99 @@
 import express from 'express';
 import { Merchant } from '../models/merchant.js';
 
-export const hunterRouter: express.Router = express.Router();
+export const merchantRouter: express.Router = express.Router();
 
-hunterRouter.post('/merchant', async (req, res) => {
+merchantRouter.post('/merchant', async (req, res) => {
   try {
-    const hunter = new Merchant(req.body);
-    await hunter.save();
-    return res.status(201).send(hunter);
+    const merchant = new Merchant(req.body);
+    await merchant.save();
+    return res.status(201).send(merchant);
   } catch (error) {
     return res.status(400).send({
-      error: 'Failed to create hunter',
+      error: 'Failed to create merchant',
     });
   }
 });
 
-hunterRouter.get('/merchant', async (req, res) => {
+merchantRouter.get('/merchant', async (req, res) => {
   try {
-    const hunters = await Merchant.find({});
-    if (!hunters.length) {
-      return res.status(404).send({ error: "There are no hunters" });
+    const merchants = await Merchant.find({});
+    if (!merchants.length) {
+      return res.status(404).send({ error: "There are no merchants" });
     }
-    return res.status(200).send(hunters);
+    return res.status(200).send(merchants);
   } catch (error) {
     return res.status(500).send({
-      error: 'Failed to fetch hunters',
+      error: 'Failed to fetch merchants',
     });
   }
 });
 
-hunterRouter.get('/merchant/:id', async (req, res) => {
+merchantRouter.get('/merchant/:id', async (req, res) => {
   try {
-    const hunter = await Merchant.findById(req.params.id);
-    if (!hunter) {
-      return res.status(404).send({ error: "Hunter not found" });
+    const merchant = await Merchant.findById(req.params.id);
+    if (!merchant) {
+      return res.status(404).send({ error: "merchant not found" });
     }
-    return res.status(200).send(hunter);
+    return res.status(200).send(merchant);
   } catch (error) {
     return res.status(500).send({
-      error: 'Failed to fetch hunter',
+      error: 'Failed to fetch merchant',
     });
   }
 });
 
-hunterRouter.get('/merchant/race/:race', async (req, res) => {
+merchantRouter.get('/merchant/profession/:profession', async (req, res) => {
   try {
-    const hunters = await Merchant.find({ race: req.params.race });
-    res.status(hunters.length ? 200 : 404).send(hunters.length ? hunters : { error: 'No hunters of that race found' });
+    const merchants = await Merchant.find({ profession: req.params.profession });
+    res.status(merchants.length ? 200 : 404).send(merchants.length ? merchants : { error: 'No merchants of that race found' });
   } catch (error) {
     res.status(500).send(error);
   }
 });
 
-hunterRouter.get('/merchant/location/:location', async (req, res) => {
+merchantRouter.get('/merchant/location/:location', async (req, res) => {
   try {
-    const hunters = await Merchant.find({ location: req.params.location });
-    res.status(hunters.length ? 200 : 404).send(hunters.length ? hunters : { error: 'No hunters in that location' });
+    const merchants = await Merchant.find({ location: req.params.location });
+    res.status(merchants.length ? 200 : 404).send(merchants.length ? merchants : { error: 'No merchants in that location' });
   } catch (error) {
     res.status(500).send(error);
   }
 });
 
-hunterRouter.patch('/merchant/:id', async (req, res) => {
+merchantRouter.patch('/merchant/:id', async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'race', 'location'];
+  const allowedUpdates = ['name', 'profession', 'location'];
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
   if (!isValidOperation) {
     return res.status(400).send({ error: 'Invalid updates!' });
   }
   try {
-    const hunter = await Merchant.findByIdAndUpdate(req.params.id, req.body, {
+    const merchant = await Merchant.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
-    if (!hunter) {
-      return res.status(404).send({ error: "Hunter not found" });
+    if (!merchant) {
+      return res.status(404).send({ error: "merchant not found" });
     }
-    return res.status(200).send(hunter);
+    return res.status(200).send(merchant);
   } catch (error) {
     return res.status(500).send({
-      error: 'Failed to update hunter',
+      error: 'Failed to update merchant',
     });
   }
 });
 
-hunterRouter.delete('/merchant/:id', async (req, res) => {
+merchantRouter.delete('/merchant/:id', async (req, res) => {
   try {
-    const hunter = await Merchant.findByIdAndDelete(req.params.id);
-    if (!hunter) {
-      return res.status(404).send({ error: "Hunter not found" });
+    const merchant = await Merchant.findByIdAndDelete(req.params.id);
+    if (!merchant) {
+      return res.status(404).send({ error: "merchant not found" });
     }
-    return res.status(200).send(hunter);
+    return res.status(200).send(merchant);
   } catch (error) {
     return res.status(500).send({
-      error: 'Failed to delete hunter',
+      error: 'Failed to delete merchant',
     });
   }
 });
