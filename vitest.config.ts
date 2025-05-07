6cee2eda68b30b492be589c52227f1194ea5659b
ValBoschP <alu0101485287@ull.edu.es>
2025-05-08 00:02:06 +0100

    fix: add `test` timeouts to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    testTimeout: 15000,
+    hookTimeout: 15000,
     coverage: {
       include: ["src/**/*"],
       exclude: ["docs/**/*", "src/inquirer/*"],
